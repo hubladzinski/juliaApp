91ca378a72bdc70274f07cbf7b13262181df2af5
hublad2 <hublad@wp.pl>
2020-05-18 12:23:59 +0200

    Images resized

--- src/index.js
+++ src/index.js
@@ -2,20 +2,6 @@ var nav = document.getElementById("navbar");
 var burger = document.getElementById("burger");
 var hasScrolled = false;
 
-//window.addEventListener("scroll", function () {
-//  if (window.scrollY == 0) {
-//    nav.classList.remove("navigation-scrolling");
-//    burger.classList.remove("menu-burger-label-scrolling");
-//    nav.classList.add("navigation");
-//    hasScrolled = false;
-//  } else {
-//    nav.classList.remove("navigation");
-//    nav.classList.add("navigation-scrolling");
-//    burger.classList.add("menu-burger-label-scrolling");
-//    hasScrolled = true;
-//  }
-//});
-
 //var checkbox = document.querySelector("#menu-burger");
 
 //checkbox.addEventListener("change", function () {
